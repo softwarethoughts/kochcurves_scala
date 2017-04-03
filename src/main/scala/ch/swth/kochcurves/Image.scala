09cb9b6dcab48ec7f081952049c309d14c43a50d
rziegler <roland.ziegler@itds.ch>
2017-04-03 16:33:29 +0200

    Refactored buildScreen

--- src/main/scala/ch/swth/kochcurves/Image.scala
+++ src/main/scala/ch/swth/kochcurves/Image.scala
@@ -7,10 +7,10 @@ import javafx.scene.paint.Color
   */
 class Image(w: Int, h: Int, c: Color, s: List[List[Color]]) {
 
-  val width = w
-  val height = h
-  val color = c
-  val screen = s
+  private val width = w
+  private val height = h
+  private val color = c
+  private val screen = s
 
   def this(width: Int, height: Int, color: Color) = this(width, height, color, List.fill(height)(List.fill(width)(color)))
 
@@ -21,33 +21,32 @@ class Image(w: Int, h: Int, c: Color, s: List[List[Color]]) {
   def fill(color: Color) = new Image(width, height, color)
 
   def line(x1: Int, y1: Int, x2: Int, y2: Int, color: Color): Image = {
-    def yValue: (Int, Int) => (Int) = y2 * _ - y2 * x1 - y1 * _ + y1 * x2 / (x2 - x1)
+    def y(x: Int): Int = y2 * x - y2 * x1 - y1 * x + y1 * x2 / (x2 - x1)
 
     def append(image: Image, color: Color) =
-      if (image.screen.last.size < width)
+      if (image.screen.last.size < width) // same row
         new Image(image.screen.init ::: List(image.screen.last ::: color :: Nil), this.color)
-      else
+      else // new row
         new Image(image.screen ::: List(color :: Nil), this.color)
 
     def buildScreen(image: Image): Image = {
-      def isPointMemberOfLine(x: Int): Boolean = yValue(x, x) == image.screen.size - 1
+      def isPixelOnLine(x: Int): Boolean = y(x) == image.screen.size - 1
 
-      def colorOfPixelinOriginalImage = if (image.screen.last.size < width) screen(actualYPosition)(actualXPosition) else screen(image.screen.size)(actualXPosition)
+      def colorOfPixelInOriginalImage = if (image.screen.last.size < width) screen(actualY)(actualX) else screen(image.screen.size)(actualX)
 
-      def actualXPosition = if (image.screen.last.size < width) image.screen.last.size else 1
+      def actualX = if (image.screen.last.size < width) image.screen.last.size else 1
 
-      def actualYPosition = image.screen.size - 1
+      def actualY = image.screen.size - 1
 
       if (image.screen.size == this.screen.size)
         image
-      else {
-        if (actualXPosition >= x1 && actualXPosition <= x2 && isPointMemberOfLine(actualXPosition))
-          buildScreen(append(image, color))
-        else
-          buildScreen(append(image, colorOfPixelinOriginalImage))
-      }
+      else if (actualX >= x1 && actualX <= x2 && isPixelOnLine(actualX))
+        buildScreen(append(image, color))
+      else
+        buildScreen(append(image, colorOfPixelInOriginalImage))
     }
 
     buildScreen(new Image(this.color))
   }
+
 }
